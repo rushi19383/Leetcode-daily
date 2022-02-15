5b7e310c37b811a52458376f4148b9017da759d8
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-02-15 08:18:14 +0530

    Create sol.cpp

---
+++ 136. Single Number/sol.cpp
@@ -0,0 +1,9 @@
+ int singleNumber(vector<int>& nums) {
+        int ans=nums[0];
+        for(int i=1;i< nums.size();i++){
+            ans=ans^nums[i];
+        }
+        return ans;
+        
+        
+    }
