9191556117e6928b1c8f2f693b4a06b83b8c7425
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-06-05 10:45:24 +0530

    Time: 23 ms (28.94%), Space: 7.3 MB (67.78%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,45 +1,106 @@
 class Solution {
-    vector<vector<string>> ans;
 public:
-    bool isSafe(vector<string> &board,int row ,int col){
-           //column check
-        for(int i = 0; i<board.size(); ++i){
-            if(board[i][col] == 'Q'){return false;}
-        }
-        int x = row; int y = col;
-		
-        //checking for upper left diagonal(non-main diagonal), row is decreasing and col too
-        while(x>=0 && y>=0){
-            if(board[x][y] == 'Q'){return false;}
-            --x;  --y; 
-        }
-        
-         //checking for main diagonal(upper right), row is decreasing and col increasing
-        while(row>=0 && col<board.size()){
-            if(board[row][col] == 'Q'){return false;}
-            --row; ++col;
-        }
-        
-        //The fn didn't returned false till , implies that our position is safe to place the queen
-        return true;
-    }
-    
-    void placeQueen(vector<string>& board,int row){
-        if(row==board.size()){
-            ans.push_back(board);
-            return;
-        }
-        for(int col=0;col<board.size();++col){
-            if(isSafe(board,row,col)){
-                board[row][col]='Q';
-                placeQueen(board,row+1);
-                board[row][col]='.';
+    vector<string>a;
+    vector<vector<string>> ans;
+    bool isValid(int n ,int row,int col){
+        int total=0;
+        for(int i=0;i<n;i++){
+            if(a[row][i]=='Q'){
+                total++;
+                
+                    
+                    
             }
         }
+        if(total>0){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            if(a[i][col]=='Q'){
+                total++;
+            }
+            
+        }
+        if(total>0){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            if(row+i<n && col+i<n){
+                if(a[row+i][col+i]=='Q'){
+                    total++;
+                }
+            }else{
+                break;
+            }
+        }
+        if(total>0){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            if(row-i>=0 && col-i>=0){
+                if(a[row-i][col-i]=='Q'){
+                    total++;
+                }
+            }else{
+                break;
+            }
+        }
+        if(total>0){
+            return false;
+        }
+        
+        
+        for(int i =0;i<n;i++){
+            if(row+i<n && col-i>=0){
+                if(a[row+i][col-i]=='Q'){
+                    total++;
+                }
+            }else{
+                break;
+            }
+            
+        }
+        if(total>0){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            if(row-i>=0 && col+i<n){
+                if(a[row-i][col+i]=='Q'){
+                    total++;
+                }
+            }else{
+                break;
+            }
+        }
+        if(total>0){
+            return false;
+        }
+        
+        return true;
+    }
+    void helper(int n,int rowNo){
+        if(rowNo == n){
+            ans.push_back(a);
+            return ;
+        }
+        for(int i=0;i<n;i++){
+            if(isValid(n,rowNo,i)){
+                a[rowNo][i] ='Q';
+                helper(n,rowNo+1);
+                a[rowNo][i]='.';
+            }
+        }
+        return;
+        
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string>board(n,string(n,'.'));
-        placeQueen(board,0);
+        string start = "";
+        for(int i=0;i<n;i++){
+            start = start+".";
+            
+        }
+        a=vector<string>(n,start);
+        helper(n,0);
         return ans;
     }
 };
