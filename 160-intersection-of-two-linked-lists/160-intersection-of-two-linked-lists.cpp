6d4b8385b98f2921d660331a5653a0ad3dddc7e6
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-06-06 14:34:31 +0530

    Time: 44 ms (94.06%), Space: 14.6 MB (72.44%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,23 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         if(headA==NULL || headB==NULL){
+//             return NULL;
+//         }
+//         ListNode* a=headA;
+//         ListNode* b=headB;
+//         while(a!=b){
+//             a=a==NULL ? headB:a->next;
+//             b=b==NULL ? headA:b->next;
+//         }
+//         return a;
+//     }
+// };
+
+
 class Solution {
+    uintptr_t acc = 0;
+
+    ListNode* reverse(ListNode *head) {
+        ListNode *prev = nullptr, *tmp;
+        while (head) {
+            acc ^= reinterpret_cast<uintptr_t> (head);
+            tmp = head->next;
+            head->next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+    
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA==NULL || headB==NULL){
-            return NULL;
-        }
-        ListNode* a=headA;
-        ListNode* b=headB;
-        while(a!=b){
-            a=a==NULL ? headB:a->next;
-            b=b==NULL ? headA:b->next;
-        }
-        return a;
+        ListNode *p = headA, *q = headB;
+        p = reverse(p);
+        q = reverse(q);
+        p = reverse(p);
+        if (q != headA) q = reverse(q);
+        return reinterpret_cast<ListNode*> (acc);
     }
 };
