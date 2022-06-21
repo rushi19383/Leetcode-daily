d2c7c54716e85e121188c60c03a88326ba34b705
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-06-21 12:47:39 +0530

    Time: 126 ms (81.23%), Space: 54 MB (27.49%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& v, int b, int l) {
+        int i=0;
+        priority_queue<int>q;
+        for(i;i<v.size()-1;i++){
+            if(v[i+1]<=v[i])continue;
+            int d=v[i+1]-v[i];
+            if(d<=b){
+                b-=d;
+                q.push(d);
+                
+            }else if(l>0){
+                if(q.size()==0){
+                    l--;
+                    continue;
+                }
+                int x=q.top();
+                if(x>d){
+                    b+=x;
+                    q.pop();
+                    q.push(d);
+                    b-=d;
+                    
+                }
+                l--;
+            }
+            else break;
+            
+        }
+        return i;
+    }
+};
