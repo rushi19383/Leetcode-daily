a88ddd0d14b2c989396416a94ac6594946fd3e86
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-07-26 12:23:03 +0530

    Time: 18 ms (69.00%), Space: 8.9 MB (93.70%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        int n=nums.size();
-        for(int i=0;i<nums.size();i++)
-            sum+=nums[i];  
-        if(sum < abs(target) || (target+sum) %2 != 0) return 0; 
-        int s1=(target+sum)/2;
-        int t[n+1][s1+1];
-		 
-        for(int i=0;i<n+1;i++){ 
-            for(int j=0;j<s1+1;j++){
-                if(i==0)
-                    t[i][j]=0;
-                if(j==0)
-                    t[i][j]=1;
+    int subsetCount(vector<int>& nums,int s){
+        int n= nums.size();
+        int t[n+1][s+1];
+        t[0][0] = 1;
+        for(int i=1;i<s+1;i++){
+            t[0][i]=0;
+            
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<s+1;j++){
+                if(nums[i-1] > j){
+                    t[i][j] = t[i-1][j];
+                }else{
+                    t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
+                }
             }
         }
-		 
-        for(int i=1;i<n+1;i++)
-            for(int j=0;j<s1+1;j++){
-                if(nums[i-1]<=j)
-                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
-                else
-                    t[i][j]=t[i-1][j];
-            }
-        return t[n][s1];   
+        return t[n][s];
     }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum =0;
+        for(int x:nums){
+            sum+=x;
+            
+        }
+        if(abs(target)> sum || (sum+target)%2 != 0){
+            return 0;
+        }
+        int s1= (sum+target)/2;
+        int ans = subsetCount(nums,s1);
+        return ans;
+    }
+    
 };
