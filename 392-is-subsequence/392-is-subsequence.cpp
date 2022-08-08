bd4fdbbe8aa17532c1222e1ff1ffd8c50b05f545
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-08-08 16:16:39 +0530

    Time: 263 ms (5.01%), Space: 50.3 MB (5.00%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,22 +1,58 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int pr=-1;
-        int c=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=0;j<t.length();j++){
-                if(s[i]==t[j] && pr<j){
-                    pr=j;
-                    c++;
-                    break;
+     int dp[1001][10001];
+    int LCS(string& x,string& y,int n,int m){
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
                 }
             }
-            
         }
-        if(c==s.length()){
-            return true;
-        }else{
-            return false;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(x[i-1]==y[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(0+dp[i][j-1],0+dp[i-1][j]);
+                }
+            }
         }
+        return dp[n][m];
+         
+    }
+    bool isSubsequence(string x, string y) {
+         int n=x.size();
+        int m=y.size();
+       
+        memset(dp,-1,sizeof(dp));
+        int ans =LCS(x,y,n,m);
+//         int i=n;int j=m;
+//         string s="";
+//         while(i>0 || j>0){
+//             if(x[i-1] == y[j-1]){
+//                 s.push_back(x[i-1]);
+//                 i--;
+//                 j--;
+//             }else{
+//                 if(dp[i][j-1] > dp[i-1][j]){
+//                     j--;
+//                 }else{
+//                     i--;
+//                 }
+//             }
+//         }
+        
+//         if(m!=0 || n!=0){
+//              reverse(s.begin(), s.end());
+//         }
+        
+     
+          
+         if(ans == x.length()){
+             return true;
+         }else{
+             return false;
+         }
     }
 };
