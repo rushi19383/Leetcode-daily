cdbbb0c840b35da502f6703860697162fab5bfd8
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-08-25 16:29:52 +0530

    Time: 40 ms (26.56%), Space: 8.7 MB (72.69%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int> mp;
+        for(char ch: magazine){
+            mp[ch]++;
+        }
+        for(char ch: ransomNote){
+            if(mp[ch]-- <=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
