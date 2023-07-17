6f68ecb20db11f62dab053d5c2d510840bf08f58
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2023-07-17 12:16:53 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,46 +10,39 @@ using namespace std;
 
 class Solution {
   public:
-  
-    bool dfsCheck(int node,vector<int>adj[],int vis[],int pathVis[],int check[]){
-        vis[node] = 1;
-        pathVis[node] =1;
-        check[node] =0;
-        
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfsCheck(it,adj,vis,pathVis,check)==true){
-                    check[node] = 0;
-                    return true;
-                }
-            }
-            else if(pathVis[it]){
-                check[node] = 0;
-                return true;
-            }
-        }
-        check[node] = 1;
-        pathVis[node]=0;
-        return false;
-    }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
         
-        int vis[V] ={0};
-        int pathVis[V] = {0};
-        int check[V] = {0};
+        vector<int>adjRev[V];
+        int indegree[V] ={0};
         
-        vector<int>safeNodes;
-        
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfsCheck(i,adj,vis,pathVis,check);
-            }
-        }
-        for(int i=0;i<V;i++){
-            if(check[i]==1) safeNodes.push_back(i);
-        }
-        return safeNodes;
+        for (int i = 0; i < V; i++) {
+			 
+			for (auto it : adj[i]) {
+				adjRev[it].push_back(i);
+				indegree[i]++;
+			}
+		}
+		queue<int> q;
+		vector<int> safeNodes;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			safeNodes.push_back(node);
+			for (auto it : adjRev[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+
+		sort(safeNodes.begin(), safeNodes.end());
+		return safeNodes;
         
         
     }
