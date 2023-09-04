576e6f1b47744a1fdd6a4ea87532fb2bd2bef20b
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2023-09-04 11:11:40 +0530

    Time: 2 ms (44.35%), Space: 6.1 MB (71.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,14 +7,20 @@ public:
             for(int j=0;j<n;j++){
                 if(i==0 || j==0){
                     dp[i][j] = 1;
+                }else{
+                    int up =0;
+                    int left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up +left;
                 }
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //     }
+        // }
         return dp[m-1][n-1];
     }
 };
