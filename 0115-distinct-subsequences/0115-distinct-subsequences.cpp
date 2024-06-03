7ffad4392615f816e9157c9b582a52beb8cfc08e
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2024-06-03 16:30:36 +0530

    Time: 21 ms (48.17%), Space: 19.6 MB (70.79%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+
+    int prime = 1e9 +7;
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+
+        }
+        for(int i=1;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                }else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
