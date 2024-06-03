cba422533fb874db3b843e3d2b31a23af69595c1
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2024-06-03 10:31:51 +0530

    Time: 20 ms (87.55%), Space: 8.1 MB (88.16%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int lcs(string s1,string s2){
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<int>prev(m+1,0),cur(m+1,0);
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1]){
+                    cur[j]=1+prev[j-1];
+
+                }else{
+                    cur[j]=max(prev[j],cur[j-1]);
+                }
+            }
+            prev = cur;
+        }
+        return prev[m];
+
+
+
+    }
+
+    int longestPalindromeSubsequence(string s){
+        string t = s;
+        reverse(s.begin(),s.end());
+        return lcs(s,t);
+    }
+    int minInsertions(string s) {
+        int n = s.size();
+        int k = longestPalindromeSubsequence(s);
+        return n-k;
+    }
+};
